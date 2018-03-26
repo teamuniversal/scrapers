e5d1c384f2d8e0a4aa998d792b6db2b4bbb464a5
Atrain <tempusatrain@gmail.com>
2018-03-26 09:12:03 -0500

    v1.0.4.5
    v1.0.4.5

--- _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/hdonline.py
+++ _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/hdonline.py
@@ -1,6 +1,6 @@
 import requests
 import re,time
-import xbmc
+import xbmcaddon
 from ..scraper import Scraper
 from ..common import clean_title,clean_search,random_agent,send_log,error_log 
 dev_log = xbmcaddon.Addon('script.module.universalscrapers').getSetting("dev_log")
