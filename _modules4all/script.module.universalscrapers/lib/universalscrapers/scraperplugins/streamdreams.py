ca4b7d4d43dd6cc50ae8efae88c7a7e2f577c97b
team_uni <scrapersuniversal@gmail.com>
2018-11-10 12:10:20 -0500

    update streamdreams
    fix streamdreams

--- _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/streamdreams.py
+++ _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/streamdreams.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # Universal Scrapers
-#checked 29/10/2018
+#checked 10/11/2018
 
-import re, requests, time, urllib
+import re, time, urllib
 import xbmcaddon, xbmc
 from universalscrapers.scraper import Scraper
-from universalscrapers.common import clean_title, clean_search, send_log, error_log
-from universalscrapers.modules import client
+from universalscrapers.common import clean_title, filter_host, clean_search, send_log, error_log
+from universalscrapers.modules import client, dom_parser as dom, cfscrape
 
 dev_log =xbmcaddon.Addon('script.module.universalscrapers').getSetting("dev_log")
 
@@ -19,20 +19,24 @@ class streamdreams(Scraper):
     def __init__(self):
         self.base_link = 'https://streamdreams.org'
         self.search_url = self.base_link + '/?s=%s'
-        self.sources = []
 
     def scrape_movie(self, title, year, imdb, debrid=False):
         try:
             start_time = time.time()
-            search_id = clean_search(title).replace(' ','+').lower()
+            search_id = urllib.quote_plus(clean_search(title))
             query = self.search_url % search_id
-            #print query
 
-            item_url = self._search(query, title, year)
-            #print '##'+ item_url+'##'
-            self.get_source(item_url, title, year, '', '', start_time)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+                'Referer': self.base_link}
+            scraper = cfscrape.create_scraper()
+            r = scraper.get(query, headers=headers).content
 
-            return self.sources
+            posts = client.parseDOM(r, 'div', attrs={'class': 'col-xs-4 col-sm-4 col-md-3 col-lg-3'})
+            posts = [dom.parse_dom(i, 'a', req='href')[0] for i in posts if year in i]
+            post = [i.attrs['href'] for i in posts if clean_title(title) == clean_title(i.attrs['title'])][0]
+            self.get_source(post, title, year, '', '', start_time)
+
         except Exception, argument:
             if dev_log == 'true':
                 error_log(self.name, 'Check Search')
@@ -41,75 +45,51 @@ class streamdreams(Scraper):
     def scrape_episode(self, title, show_year, year, season, episode, imdb, tvdb, debrid=False):
         try:
             start_time = time.time()
-            search = clean_search(title).replace(' ','-').lower()
-            start_url = '%s/shows/%s?session=%s&episode=%s' % (self.base_link, search, season, episode)
-            #print start_url
+            query = urllib.quote_plus(clean_search(title))
+            query = self.search_url % query
+
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+                'Referer': self.base_link}
+            scraper = cfscrape.create_scraper()
+            r = scraper.get(query, headers=headers).content
+
+            posts = client.parseDOM(r, 'div', attrs={'class': 'col-xs-4 col-sm-4 col-md-3 col-lg-3'})
+            posts = [dom.parse_dom(i, 'a', req='href')[0] for i in posts if show_year in i]
+            post = [i.attrs['href'] for i in posts if clean_title(title) == clean_title(i.attrs['title'])][0]
+
+            start_url = '%s?session=%01d&episode=%01d' % (post, int(season), int(episode))
             self.get_source(start_url, title, year, season, episode, start_time)
 
-            return self.sources
         except Exception, argument:
             if dev_log == 'true':
                 error_log(self.name, 'Check Search')
             return self.sources
 
-    def _search(self, url, title, year):
-        try:
-            r = client.request(url)
-            #print r
-            mov_link = client.parseDOM(r, 'div', attrs={'class': 'thumbnail.+?'})
-            #print mov_link
-            for a_s in mov_link:
-                #print a_s
-                item_url = client.parseDOM(a_s, 'a', ret='href')[0]
-                #print item_url+'?????????????'    
-                data = client.parseDOM(r, 'div', attrs={'class': 'caption thumb_caption'})
-                for item in data:
-                    name = client.parseDOM(item, 'div')[0]
-                    rel = client.parseDOM(item, 'div')[1]
-                    #print name+ ' '+ rel
-                    #print '@#@(grabbed url) %s  (name) %s' % (item_url,name)
-                    if not clean_title(title).lower() in clean_title(name).lower():
-                        continue
-                    if not year == rel:
-                        continue
-                    #print '@#@URL check> ' + item_url
-                return item_url
-        except:
-            return
-
     def get_source(self, item_url, title, year, season, episode, start_time):
         try:
             count = 0
-            r = client.request(item_url)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+                'Referer': self.base_link}
+            scraper = cfscrape.create_scraper()
+            r = scraper.get(item_url, headers=headers).content
             data = client.parseDOM(r, 'tr')
             for item in data:
-                info = client.parseDOM(item,'td')
-                for items in info:
-                    #print'>>>>>>>>>>>>'+items
-                    qual = client.parseDOM(items,'span', ret= 'class')[0] 
-                    if 'quality' in qual:
-                        qual=qual.replace('quality_','')
-                        #print qual
-                        links = client.parseDOM(item, 'a', ret='href')
-                        links = [i for i in links if not '/report/' in i]
-                        for link in links:
-                            #print link
-                            count +=1
-                            #print link + qual
-                            host = link.split('//')[1].replace('www.','')
-                            host = host.split('/')[0].split('.')[0].title() 
-                            self.sources.append({'source': host, 'quality': qual, 'scraper': self.name, 'url': link, 'direct': False})
+                qual = client.parseDOM(item, 'span', ret='class')[0]
+                qual = qual.replace('quality_', '')
+
+                link = client.parseDOM(item, 'a', ret='data-href')[0]
+
+                host = link.split('//')[1].replace('www.', '')
+                host = host.split('/')[0]
+                if not filter_host(host): continue
+                count += 1
+                self.sources.append({'source': host, 'quality': qual, 'scraper': self.name, 'url': link, 'direct': False})
             if dev_log == 'true':
                 end_time = time.time() - start_time
                 send_log(self.name, end_time, count, title, year, season='', episode='')
-        except:
-            pass
 
-    def resolve(self, link):
-        try:
-            url = client.request(link)
-            url = re.findall('''gotourl\(['"](.+?)['"]\)''', url)[0]
-            #xbmc.log("$#$SCRAPER-RESOLVE-LINK:%s" % url, xbmc.LOGNOTICE)
-            return url
-        except:
-            return link
+            return self.sources
+        except BaseException:
+            return self.sources
