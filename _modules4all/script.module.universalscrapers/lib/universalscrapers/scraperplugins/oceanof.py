d9771ee691885f8a4519c1f9cb9b82bd2cb159cc
team_uni <scrapersuniversal@gmail.com>
2018-11-10 12:55:00 -0500

    fix oceanof
    fix oceanof

--- _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/oceanof.py
+++ _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/oceanof.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Universal Scrapers
-# checked 2/11/2018 -BUG
+# checked 10/11/2018 -BUG
 
 import urllib, re, time
 import xbmc, xbmcaddon
@@ -24,56 +24,53 @@ class joymovies(Scraper):
             start_time = time.time()
             search_id = '%s %s' % (title, year)
             start_url = self.base_link + self.search_link % urllib.quote_plus(search_id)
-            #print 'STARTURL:::::::::::::::: '+start_url
 
             r = client.request(start_url)
             items = client.parseDOM(r, 'item')
+            items = [i for i in items if imdb in i]
             for item in items:
-                name = re.search('Movie\s*Name\s*:\s*(.+?)<br', item, re.DOTALL).groups()[0]
-                name = client.replaceHTMLCodes(name)
-                t = re.sub('(\.|\(|\[|\s)(\d{4})(\.|\)|\]|\s|)(.+|)', '', name, re.I)
-
-                if not clean_title(title) == clean_title(t) and year not in name:
-                    continue
-
                 self.get_source(item, title, year, '', '', start_time)
 
             return self.sources
         except Exception, argument:        
             if dev_log == 'true':
-                error_log(self.name,argument)
+                error_log(self.name, argument)
             return self.sources
 
     def get_source(self, item_url, title, year, season, episode, start_time):
+        count = 0
         try:
             #print '%s %s %s' %(item_url,title,year)
-
-            qual = re.search('uality\s*:(.+?)<br', item_url, re.DOTALL).groups()[0]
+            if item_url is None:
+                return self.sources
+            qual = re.search('Quality\s*:(.+?)<br', item_url, re.DOTALL).groups()[0]
+            qual = re.sub('<.+?>', '', qual)
             qual, info = quality_tags.get_release_quality(qual, qual)
 
-            count = 0
+
             headers = {'Origin': self.base_link, 'Referer': client.parseDOM(item_url, 'link')[0],
                        'X-Requested-With': 'XMLHttpRequest', 'User_Agent': client.agent()}
-            try:
-                fn = client.parseDOM(item_url, 'input', attrs={'name': 'FName'}, ret='value')[0]
-                fs = client.parseDOM(item_url, 'input', attrs={'name': 'FSize'}, ret='value')[0]
-                fsid = client.parseDOM(item_url, 'input', attrs={'name': 'FSID'}, ret='value')[0]
-                #params = re.compile('<input name="FName" type="hidden" value="(.+?)" /><input name="FSize" type="hidden" value="(.+?)" /><input name="FSID" type="hidden" value="(.+?)"').findall(html)
 
-                post_url = self.base_link + '/thanks-for-downloading/'
-                form_data = {'FName': fn, 'FSize': fs, 'FSID': fsid}
-                #link = requests.post(request_url, data=form_data, headers=headers).content
-                link = client.request(post_url, post=form_data, headers=headers)
+            fn = client.parseDOM(item_url, 'input', attrs={'name': 'FName'}, ret='value')[0]
+            fs = client.parseDOM(item_url, 'input', attrs={'name': 'FSize'}, ret='value')[0]
+            fsid = client.parseDOM(item_url, 'input', attrs={'name': 'FSID'}, ret='value')[0]
+            #params = re.compile('<input name="FName" type="hidden" value="(.+?)" /><input name="FSize" type="hidden" value="(.+?)" /><input name="FSID" type="hidden" value="(.+?)"').findall(html)
 
-                stream_url = client.parseDOM(link, 'meta', attrs={'http-equiv': 'refresh'}, ret='content')[0]
-                stream_url = re.search('url=(.+?)"', stream_url, re.DOTALL).groups()[0]
-            except:
-                pass
-            stream_url = client.replaceHTMLCodes(stream_url).replace('1; url=','')+'|User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:61.0) Gecko/20100101 Firefox/61.0'#stream_url.replace('#038;','')
-            count +=1
-            self.sources.append({'source': 'DirectLink', 'quality': qual, 'scraper': self.name,'url': stream_url, 'direct': True})
+            post_url = self.base_link + '/thanks-for-downloading/'
+            form_data = {'FName': fn, 'FSize': fs, 'FSID': fsid}
+            #link = requests.post(request_url, data=form_data, headers=headers).content
+            link = client.request(post_url, post=form_data, headers=headers)
+
+            stream_url = client.parseDOM(link, 'meta', attrs={'http-equiv': 'refresh'}, ret='content')[0]
+            stream_url = client.replaceHTMLCodes(stream_url).split('url=')[-1]
+
+            stream_url += '|User-Agent=%s' % urllib.quote(client.agent())
+            count += 1
+            self.sources.append({'source': 'DirectLink', 'quality': qual, 'scraper': self.name, 'url': stream_url, 'direct': True})
+
             if dev_log == 'true':
                 end_time = time.time() - start_time
                 send_log(self.name, end_time, count, title + stream_url, year, season=season, episode=episode)
+
         except:
             pass
