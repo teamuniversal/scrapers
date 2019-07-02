469cc26dd61cead973db1d6e88c2a34c571267be
tkey3 <dbtng@live.co.uk>
2019-07-02 20:58:21 +0100

    v1.1.7

--- _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/gowatchseries.py
+++ _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/gowatchseries.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # 30/10/2018 -BUG
 
-import requests
 import re, xbmcaddon, time
-import urllib, urlparse
+import urllib, urlparse, requests
 from universalscrapers.scraper import Scraper
 from universalscrapers.common import clean_title, clean_search, filter_host, send_log,error_log
 from universalscrapers.modules import client
@@ -12,65 +11,55 @@ dev_log = xbmcaddon.Addon('script.module.universalscrapers').getSetting("dev_log
 
 
 class thewatchseries(Scraper):
-    domains = ['gowatchseries.co/']
-    name = "TheWatchSeries"
+    domains = ['gowatchseries.co', 'gowatchseries.tv']
+    name = "GoWatchSeries"
     sources = []
 
+
     def __init__(self):
-        self.base_link = 'https://ww5.gowatchseries.co/'
+        self.base_link = 'https://gowatchseries.tv/'
         self.sources = []
+
 
     def scrape_movie(self, title, year, imdb, debrid=False):
         try:
             start_time = time.time()
-            #scraper = cfscrape.create_scraper()
             scrape = urllib.quote_plus(clean_search(title))
             start_url = '%s/search.html?keyword=%s' % (self.base_link, scrape)
-            #print 'SEARCH  > '+start_url
-
             html = client.request(start_url)
-
             thumbs = re.compile('<ul class="listing items">(.+?)</ul> ',re.DOTALL).findall(html)
             thumb = re.compile('href="(.+?)".+?alt="(.+?)"',re.DOTALL).findall(str(thumbs))
-
             for link, link_title in thumb:
                 if clean_title(title).lower() == clean_title(link_title).lower():
-                    #print "<<<<<<<<<<<<<link>>>>>>>>>>"+link
                     page_link = urlparse.urljoin(self.base_link, link)
-
                     holdpage = client.request(page_link)
                     datecheck = re.compile('<span>Release: </span>(.+?)</li>',re.DOTALL).findall(holdpage)[0]
                     if year in datecheck:
                         movie_link = re.compile('<li class="child_episode".+?href="(.+?)"',re.DOTALL).findall(holdpage)[0]
                         movie_link = urlparse.urljoin(self.base_link, movie_link)
-                        #print 'GW >>>'+movie_link
                         self.get_source(movie_link,title,year,'','',start_time)
                     else:pass
-            #print self.sources
             return self.sources
         except Exception, argument:        
             if dev_log == 'true':
                 error_log(self.name,argument)
             return self.sources
 
+
     def scrape_episode(self, title, show_year, year, season, episode, imdb, tvdb, debrid = False):
         try:
             start_time = time.time()
             scrape = urllib.quote_plus(clean_search(title))
             start_url = '%s/search.html?keyword=%s' %(self.base_link, scrape)
-            #print 'SEARCH  > '+start_url
-
             html = client.request(start_url)
             thumbs = re.compile('<ul class="listing items">(.+?)</ul> ',re.DOTALL).findall(html)
             thumb = re.compile('href="(.+?)".+?alt="(.+?)"',re.DOTALL).findall(str(thumbs))  
             for link, link_title in thumb:
                 if clean_title(title) in clean_title(link_title):
                     season_chk = '-season-%s' %season
-                    #print 'season chk% '+season_chk
-                    if not season_chk in link: continue
+                    if not season_chk in link:
+                        continue
                     page_link = urlparse.urljoin(self.base_link, link)
-                    #print 'page_link:::::::::::::: '+page_link
-
                     holdpage = client.request(page_link)
                     series_links = re.compile('<li class="child_episode".+?href="(.+?)"',re.DOTALL).findall(holdpage)
                     for movie_link in series_links:
@@ -78,43 +67,23 @@ class thewatchseries(Scraper):
                         spoof_link = movie_link + 'BOLLOX'
                         if episode_chk in spoof_link:
                             movie_link = urlparse.urljoin(self.base_link, movie_link)
-                            #print 'pass TWS episode check: '+movie_link
                             self.get_source(movie_link,title,year,season,episode,start_time)
-                    else:pass
+                    else:
+                        pass
             return self.sources
         except Exception, argument:        
             if dev_log == 'true':
                 error_log(self.name,argument)
 
+
     def get_source(self, movie_link, title, year, season, episode, start_time):
-        #print '###'+movie_link
         try:
             html = client.request(movie_link)
             links = client.parseDOM(html, 'li', ret='data-video')
-
             count = 0 
             for link in links:
-                #print '::::::::::::::::::::::final link> ' + link
                 if 'vidcloud' in link:
                     continue
-                    #link = 'https:' + link
-                    #page = client.request(link,timeout=10)
-                    #try:
-                    #    grab = re.compile("sources.+?file: '(.+?)',label: '(.+?)'",re.DOTALL).findall(page)
-                    #    for end_link,rez in grab:
-                    #        if '1080' in rez:
-                    #            res = '1080p'
-                    #        elif '720' in rez:
-                    #            res= '720p'
-                    #        else: res = 'unknown'
-                    #
-                    #        count +=1
-                    #        self.sources.append({'source': 'Vidnode','quality': res,'scraper': self.name,'url': end_link,'direct': True})
-                    #except:pass
-                        # vid_url = re.compile("sources.+?file: '(.+?)'",re.DOTALL).findall(page)[0]
-                        # vid_url = 'http:'+vid_url
-                        # #count +=1
-                        # self.sources.append({'source': 'GoogleLink','quality': '720p','scraper': self.name,'url': vid_url,'direct': True})
                 elif 'openload' in link:
                     try:
                         chk = requests.get(link).content
@@ -149,4 +118,3 @@ class thewatchseries(Scraper):
                 error_log(self.name,argument)
 
 
-#thewatchseries().scrape_movie('Deadpool 2', '2018', '', False)
---
+++ _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/kickass2.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# Universal Scrapers
+# checked 01/7/2019
+
+import re
+import urllib
+import xbmc, xbmcaddon, time
+from universalscrapers.common import clean_title, clean_search, send_log, error_log
+from universalscrapers.scraper import Scraper
+from universalscrapers.modules import client, quality_tags
+
+dev_log = xbmcaddon.Addon('script.module.universalscrapers').getSetting("dev_log")            
+
+
+class kickass2(Scraper):
+    domains = ['kickass2.cc']
+    name = "kickass2"
+    sources = []
+
+    def __init__(self):
+        self.base_link = 'https://kickass2.online'
+        self.search_link = 'https://kickass2.cc/usearch/{0}'
+
+    def scrape_movie(self, title, year, imdb, debrid = False):
+        try:
+            start_time = time.time()
+            search_id = clean_search(title.lower())
+            start_url = '%s/usearch/%s %s' % (self.base_link, urllib.quote_plus(search_id),year)
+            #print start_url+'>>>>>>>>>>>>>>>>>>>'
+            self.get_source(start_url, title, year, '', '', start_time)
+            
+            return self.sources
+        except Exception, argument:        
+            if dev_log == 'true':
+                error_log(self.name,argument)
+            return self.sources
+            
+    def get_source(self,start_url,title,year,season,episode,start_time):
+        try:
+            #print 'URL PASSED OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK'+start_url
+            count = 0
+            headers = {'User-Agent': client.agent()}
+            r = client.request(start_url, headers=headers)
+            #print r
+            Endlinks=re.compile('class="iaconbox center floatright".+?title="Torrent magnet link" href="(.+?)">.+?class="cellMainLink">(.+?)</a>.+?class="nobr center">(.+?)</span></td>',re.DOTALL).findall(r)
+            #print 'scraperchk - scrape_movie - EndLinks: '+str(Endlinks)
+            for Magnet, quality, size in Endlinks:
+                Magnet=Magnet.replace('https://mylink.me.uk/?url=', '')
+                qual = quality_tags.check_sd_url(quality)
+                #print Magnet + '<><><><><>'
+                count+=1
+                self.sources.append({'source':'Torrent', 'quality':size+' '+qual, 'scraper':self.name, 'url':Magnet, 'direct':False, 'debridonly': True})
+            if dev_log=='true':
+                end_time = time.time() - start_time
+                send_log(self.name,end_time,count,title,year)
+        except Exception, argument:
+            if dev_log=='true':
+                error_log(self.name,argument)
+            return[]
