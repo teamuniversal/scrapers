396068e8acfe4614220b9aa42c990fde3a9f261f
Goofball <dbtng@live.co.uk>
2019-11-06 21:29:42 +0000

    v1.2.1

--- _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/idop.py
+++ _modules4all/script.module.universalscrapers/lib/universalscrapers/scraperplugins/idop.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 # Universal Scrapers
-# checked 06/07/2019
+# checked 29/7/2019
 
 import re
 import urllib
 import xbmc, xbmcaddon, time
 from universalscrapers.common import clean_title, clean_search, send_log, error_log
 from universalscrapers.scraper import Scraper
-from universalscrapers.modules import client, quality_tags, cfscrape
+from universalscrapers.modules import client, quality_tags
 
 dev_log = xbmcaddon.Addon('script.module.universalscrapers').getSetting("dev_log")            
 
 
-class piriro(Scraper):
-    domains = ['pirateiro.com']
-    name = "PirateIro"
+class idop(Scraper):
+    domains = ['https://idope.se/']
+    name = "Idope"
     sources = []
 
     def __init__(self):
-        self.base_link = 'http://pirateiro.com'
-        self.search_link = ''
+        self.base_link = 'https://idope.se'
+        self.search_link = '%s/torrent-list/%s'
 
     def scrape_movie(self, title, year, imdb, debrid = False):
         try:
@@ -27,7 +27,7 @@ class piriro(Scraper):
             if not debrid:
                 return self.sources
             search_id = clean_search(title.lower())
-            start_url = '%s/torrents/?search=%s+%s' % (self.base_link, urllib.quote_plus(search_id),year)
+            start_url = '%s/torrent-list/%s' % (self.base_link, urllib.quote_plus(search_id))
             #print start_url+'>>>>>>>>>>>>>>>>>>>'
             self.get_source(start_url, title, year, '', '', start_time)
             
@@ -36,8 +36,7 @@ class piriro(Scraper):
             if dev_log == 'true':
                 error_log(self.name,argument)
             return self.sources
-
-
+    
     def scrape_episode(self, title, show_year, year, season, episode, imdb, tvdb, debrid=False):
         try:
             start_time = time.time()
@@ -46,32 +45,35 @@ class piriro(Scraper):
             hdlr = 'S%02dE%02d' % (int(season), int(episode))
             query = '%s+S%02dE%02d' % (urllib.quote_plus(title), int(season), int(episode))
             query = query.replace('%27','')
-            start_url='%s/torrents/?search=%s' %(self.base_link, query)
+            start_url='%s/torrent-list/%s' %(self.base_link, query)
             #print start_url
             self.get_source(start_url, title, show_year, season, episode, str(start_time))
             return self.sources
         except Exception, argument:
             if dev_log == 'true':
                 error_log(self.name, argument)
-            return self.sources
-            
+            return self.sources        
+    
     def get_source(self,start_url,title,year,season,episode,start_time):
         try:
             #print 'URL PASSED OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK'+start_url
             count = 0
             headers = {'User-Agent': client.agent()}
-            #scraper= cfscrape.create_scraper()
-            #r=scraper.get(start_url, headers=headers)
             r = client.request(start_url, headers=headers)
             #print r
-            Endlinks=re.compile('class="imagnet icon16"   href="(.+?)">.+?<font color=#004E98>(.+?)</font>.+?><b>(.+?)</b></a',re.DOTALL).findall(r)
+            Endlinks=re.compile('class="resultdiv".+?<a href="(.+?)".+?class="resultdivtopname" >(.+?)</div></a>.+?class="resultdivbottonlength">(.+?)</div>',re.DOTALL).findall(r)
             #print 'scraperchk - scrape_movie - EndLinks: '+str(Endlinks)
-            for Magnet,size, quality in Endlinks:
-                #Magnet=Magnet.replace('https://mylink.me.uk/?url=', '')
-                qual = quality_tags.get_release_quality(quality, None)[0]
-                #print Magnet + '<><><><><>'
+            for nxtpg, info, size in Endlinks:
+                nxtpg= self.base_link+nxtpg
+                info= info.lstrip()
+                #print nxtpg + '<><><><><>'+size+'><><><>'+info
+                qual = quality_tags.get_release_quality(info, None)[0]
+                #print qual
+                nxtpg=nxtpg.split('torrent/')[1].split('/')[1]
+                #print nxtpg
+                Magnet='magnet:?xt=urn:btih:'+nxtpg
                 count+=1
-                self.sources.append({'source':'Torrent', 'quality':qual+' '+size, 'scraper':self.name, 'url':Magnet, 'direct':False, 'debridonly': True})
+                self.sources.append({'source':'Torrent', 'quality':qual +' '+ size, 'scraper':self.name, 'url':Magnet, 'direct':False, 'debridonly': True})
             if dev_log=='true':
                 end_time = time.time() - start_time
                 send_log(self.name,end_time,count,title,year)
